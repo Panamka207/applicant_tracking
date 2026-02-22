3fd8ef7c4c931704b6d75744541becf71ae2ebce
reggina-hodyreva <hrr23@gmail.com>
2026-02-22 23:44:18 +0500

    Упрощена загрузка интерфейса и исправлены ошибки поиска кнопки добавления пациента

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-import sys
 from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
+import sys
+
+
+def load_ui(path):
+    loader = QUiLoader()
+    ui_file = QFile(path)
+    ui_file.open(QFile.ReadOnly)
+    window = loader.load(ui_file)
+    ui_file.close()
+    return window
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    loader = QUiLoader()
-    ui_file = QFile("./ui/main.ui")  # или "ui/main.ui"
+    main_window = load_ui("ui/main.ui")
 
-    if not ui_file.open(QFile.ReadOnly):
-        print("Не удалось открыть main.ui")
-        sys.exit(-1)
+    # НАЙДЕМ КНОПКУ
+    btn_add = main_window.findChild(type(main_window), "btnAddPatient")
 
-    window = loader.load(ui_file)
-    ui_file.close()
+    # ПРАВИЛЬНЫЙ способ:
+    from PySide6.QtWidgets import QPushButton
+    btn_add = main_window.findChild(QPushButton, "btnAddPatient")
 
-    window.show()
+    def open_patient_dialog():
+        dialog = load_ui("ui/patient_dialog.ui")
+        dialog.exec()
 
+    btn_add.clicked.connect(open_patient_dialog)
+
+    main_window.show()
     sys.exit(app.exec())
