eee266cd86ef42f5863e4e4c36cbdbb6e32287e1
reggina-hodyreva <hrr23@gmail.com>
2026-03-03 11:59:21 +0500

    Реализован основной интерфейс приложения с обновлением структуры и добавлением новых элементов управления; удалены устаревшие файлы диалогов.

--- main.py
+++ main.py
@@ -1,35 +1,57 @@
-from PySide6.QtWidgets import QApplication
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
-import sys
 
 
 def load_ui(path):
     loader = QUiLoader()
     ui_file = QFile(path)
-    ui_file.open(QFile.ReadOnly)
+
+    if not ui_file.open(QFile.ReadOnly):
+        raise RuntimeError(f"Не удалось открыть UI файл: {path}")
+
     window = loader.load(ui_file)
     ui_file.close()
     return window
 
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Загружаем UI
+        self.ui = load_ui("ui/main_window.ui")
+        self.setCentralWidget(self.ui)
+
+        # Получаем stackedWidget
+        self.stacked = self.ui.findChild(
+            type(self.ui.stackedWidget), "stackedWidget")
+
+        # Подключаем кнопки меню
+        self.ui.btnPageApplicants.clicked.connect(
+            lambda: self.stacked.setCurrentIndex(0)
+        )
+        self.ui.btnPageDirections.clicked.connect(
+            lambda: self.stacked.setCurrentIndex(1)
+        )
+        self.ui.btnPageExams.clicked.connect(
+            lambda: self.stacked.setCurrentIndex(2)
+        )
+        self.ui.btnPageReports.clicked.connect(
+            lambda: self.stacked.setCurrentIndex(3)
+        )
+
+        # Открываем первую страницу по умолчанию
+        self.stacked.setCurrentIndex(0)
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    main_window = load_ui("ui/main.ui")
+    window = MainWindow()
+    window.setWindowTitle("Учет абитуриентов")
+    window.resize(1300, 800)
+    window.show()
 
-    # НАЙДЕМ КНОПКУ
-    btn_add = main_window.findChild(type(main_window), "btnAddPatient")
-
-    # ПРАВИЛЬНЫЙ способ:
-    from PySide6.QtWidgets import QPushButton
-    btn_add = main_window.findChild(QPushButton, "btnAddPatient")
-
-    def open_applicant_dialog():
-        dialog = load_ui("ui/applicant_dialog.ui")
-        dialog.exec()
-
-    btn_add.clicked.connect(open_applicant_dialog)
-
-    main_window.show()
     sys.exit(app.exec())
